4ecef974ad654b56880683631ecc36ab2151a615
Nataniel López <nataniel.lopez@fizzmod.com>
2020-01-28 16:07:03 -0300

    inited with package-generator

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { Solr } = require('./lib');
+
+module.exports = Solr;
---
+++ tests/solr-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const assert = require('assert');
+
+const sandbox = require('sinon').createSandbox();
+
+const Solr = require('./../index');
+
+const SolrError = require('./../lib/solr-error')
+
+describe('Solr', () => {
+
+	// your tests here...
+
+});
