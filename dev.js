a82f0a0f8e63bc465c0662776100e5d55e2a4166
Nataniel López <nataniel.lopez@fizzmod.com>
2020-01-30 18:05:10 -0300

    Added basic filters support

--- dev.js
+++ dev.js
@@ -10,18 +10,41 @@ class Model {
 	static get table() {
 		return 'dev';
 	}
+
+	static get fields() {
+		return {
+			some: true,
+			other: { type: 'string' },
+			another: { type: 'number' }
+		};
+	}
 }
 
 const model = new Model();
 
+const Filters = require('./lib/helpers/filters');
+
 (async () => {
 
-	console.log(await solr.multiInsert(model, [
+	console.log(Filters.build({
+		field: { type: 'equal', value: 'sarasa' },
+		otherField: { type: 'notEqual', value: 'sarasa' },
+		anotherField: { type: 'greater', value: 5200 },
+		loquesea: { type: 'greaterOrEqual', value: 54 },
+		miau: { type: 'lesser', value: 65 },
+		guau: { type: 'lesserOrEqual', value: 32 }
+	}));
+
+	// console.log(await solr.get(model));
+
+	// .log(await solr.updateSchemas(model));
+
+	/* console.log(await solr.multiInsert(model, [
 		{
 			some: 'thing'
 		}
 	]));
 
-	console.log(await solr.get(model));
+	console.log(await solr.get(model)); */
 
 })();
