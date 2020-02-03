024f734d34ef63ba901d00f9af9e1138243b9fef
Nataniel López <nataniel.lopez@fizzmod.com>
2020-02-03 17:56:16 -0300

    Added get params support: sorting, paging, limits and filters

--- dev.js
+++ dev.js
@@ -13,25 +13,16 @@ class Model {
 
 	static get fields() {
 		return {
-			some: true,
-			other: { type: 'string' },
-			another: { type: 'number' },
-			somearr: { type: ['string'] },
-			otherarr: { type: ['number'] },
-			someobj: {
+			text: true,
+			numeric: { type: 'number' },
+			array: { type: ['string'] },
+			arrayOfNumbers: { type: ['number'] },
+			date: { type: 'date' },
+			object: {
 				type: {
-					prop: 'string',
-					otherprop: 'number',
-					anotherprop: ['date'],
-					superprop: {
-						magicprop: ['string']
-					},
-					damnprop: {
-						damneditem: 'string',
-						otherdamneditem: ['number'],
-						superdamndeditem: {
-							ultradamneditem: 'date'
-						}
+					property: 'string',
+					otherProperty: {
+						subproperty: 'number'
 					}
 				}
 			}
@@ -41,37 +32,28 @@ class Model {
 
 const model = new Model();
 
-const Filters = require('./lib/helpers/filters');
+(async () => {
 
-const Schemas = require('./lib/helpers/schema');
+	// console.log(await solr.updateSchemas(model));
 
-const ResponseFormatter = require('./lib/helpers/res-formatter');
-
-console.log(
-	JSON.stringify(
-		ResponseFormatter.format([
-			{
-				some: 'field',
-				'jsonfield.prop': 'value',
-				'jsonfield.subprop.prop': 'othervalue',
-				'jsonfield.subprop.otherprop': 14,
-				id: '51d1ba30-8596-46e8-b4ae-b83190dd6ac5',
-				_version_: 1657249623905927168
+	console.log(await solr.insert(model, {
+		text: 'sarasa',
+		numeric: 22,
+		array: ['sarasa'],
+		arrayOfNumbers: [1, 2, 3],
+		date: new Date()
+		/* object: {
+			property: 'foobar',
+			otherProperty: {
+				subproperty: 17
 			}
-		]), null, 2
-	)
-);
-
-/* (async () => {
-
-	console.log(Filters.build({
-		field: { type: 'equal', value: 'sarasa' },
-		otherField: { type: 'notEqual', value: 'sarasa' },
-		anotherField: { type: 'greater', value: 5200 },
-		loquesea: { type: 'greaterOrEqual', value: 54 },
-		miau: { type: 'lesser', value: 65 },
-		guau: { type: 'lesserOrEqual', value: 32 }
+		} */
 	}));
 
+	console.log(
+		await solr.get(model, {
 
-})(); */
+		})
+	);
+
+})();
